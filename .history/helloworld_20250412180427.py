edbd64bc2829dd4fbba1ec75429b923e5f92d880
nathanchamorro1 <160960078+nathanchamorro1@users.noreply.github.com>
2025-04-12 19:08:26 -0700

    tea

--- .history/helloworld_20250412180427.py
+++ .history/helloworld_20250412180427.py
@@ -1,25 +1,11 @@
 from tkinter import *
 from tkinter import ttk
-
-def calculate(*args):
-    try:
-        value = float(feet.get())
-        meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
-    except ValueError:
-        pass
-
 root = Tk()
 root.title("Posture Corrector")
-
 mainframe = ttk.Frame(root, padding="3 3 12 12")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
-
-feet = StringVar()
-feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
-feet_entry.grid(column=2, row=1, sticky=(W, E))
-
 meters = StringVar()
 ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
 
@@ -28,11 +14,4 @@ ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3,
 ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
 ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
 ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
-
-for child in mainframe.winfo_children(): 
-    child.grid_configure(padx=5, pady=5)
-
-feet_entry.focus()
-root.bind("<Return>", calculate)
-
 root.mainloop()
